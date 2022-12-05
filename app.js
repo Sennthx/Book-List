bfee42bdb631afa00154deb289cce93613be1dd7
Sennthx <totharon1173@gmail.com>
2022-12-05 16:16:50 +0100

    basically finished everything, only works in ui not saved into local storage

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const form = document.querySelector("form");
+const clearAllBtn = document.querySelector("#clear-all-btn");
 
 form.addEventListener("submit", addListItem);
+clearAllBtn.addEventListener("click", clearAllItems);
+document.querySelector(".result-container").addEventListener("click", clearItem);
 
-function formCheck(e) {
+
+function formCheck() {
     // Getting UI elements
     const title = document.querySelector(".book-input");
     const author = document.querySelector(".author-input");
     const isbn = document.querySelector(".isbn-input");
-    if(title.value !== "" && author.value !== "" && isbn.value !== "") {
-        if(!isNaN(parseInt(isbn.value))) {
-            if(isbn.value.length === 13) {
+    if (title.value !== "" && author.value !== "" && isbn.value !== "") {
+        if (!isNaN(parseInt(isbn.value))) {
+            if (isbn.value.length === 13) {
                 return true;
             } else sendMessage("The ISBN number needs to be 13 characters long!", 0);
         } else sendMessage("The ISBN number cannot contain any letters!", 0);
     } else sendMessage("Please fill out the form first!", 0);
-
 }
 
 function addListItem(e) {
     e.preventDefault();
-    if(formCheck()) {
-        // Getting UI elements
+    if (formCheck()) {
+        // Getting UI elementsí
         const title = document.querySelector(".book-input");
         const author = document.querySelector(".author-input");
         const isbn = document.querySelector(".isbn-input");
@@ -28,27 +31,39 @@ function addListItem(e) {
         // Creating items
         const div = document.createElement("div");
         div.className = "main-results-item";
-        
+
         div.innerHTML = `<div class="title-items">${title.value}</div>
                          <div class="title-items">${author.value}</div>
                          <div class="title-items">${isbn.value}</div>
                          <div class="title-items"><i class="fa-solid fa-xmark fa-lg" id="delete-icon"></i></div>`;
-        
+
         // Getting results div
         resultsContainer = document.querySelector(".result-container").appendChild(div);
     }
 }
 
 let myTimeoutID;
-
 function sendMessage(text, color) {
     const messageBox = document.querySelector(".messageBox");
     const message = document.querySelector("#message");
 
-    color === 0 ? messageBox.style.backgroundColor  = "rgb(255, 0, 0, 0.3)" : messageBox.style.backgroundColor  = "rgb(0, 255, 0, 0.1)";
+    color === 0 ? messageBox.style.backgroundColor = "rgb(255, 0, 0, 0.3)" : messageBox.style.backgroundColor = "rgb(0, 255, 0, 0.1)";
     message.textContent = text;
     messageBox.style.display = "flex";
     clearTimeout(myTimeoutID);
     myTimeoutID = setTimeout(() => messageBox.style.display = "none", 3000);
-    
+}
+
+function clearAllItems() {
+    const itemList = document.querySelector(".result-container").children;
+    for (let i = 1; i < itemList.length; i++) {
+        itemList[i].remove();
+        i--;
+    }
+}
+
+function clearItem(e) {
+    if (e.target.id === "delete-icon" ){
+        e.target.parentElement.parentElement.remove();
+    }
 }
